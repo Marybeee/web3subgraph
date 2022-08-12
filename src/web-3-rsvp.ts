4378830357820efdf9e596281e0246eb599e518f
Marybeee <maria.behappy.mb@gmail.com>
2022-08-12 16:36:01 +0200

    added ipfs link

--- src/web-3-rsvp.ts
+++ src/web-3-rsvp.ts
@@ -29,7 +29,8 @@ export function handleNewEventCreated(event: NewEventCreated): void {
         const name = value.get("name");
         const description = value.get("description");
         const link = value.get("link");
-
+        const imagePath = value.get("image");
+        
         if (name) {
           newEvent.name = name.toString();
         }
@@ -40,6 +41,15 @@ export function handleNewEventCreated(event: NewEventCreated): void {
 
         if (link) {
           newEvent.link = link.toString();
+        }
+
+        if(imagePath){
+          const imageURL =
+      "https://ipfs.io/ipfs/" + event.params.eventDataCID + imagePath.toString();
+          newEvent.imageURL = imageURL;
+        } else {
+          const fallbackURL = "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+          newEvent.imageURL = fallbackURL;
         }
       }
     }
